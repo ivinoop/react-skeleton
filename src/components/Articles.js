99248d6a6945e02f1d01769db676c6e14fa11257
Vinoo Prakash <vinoo@southpawstudio.co>
2023-02-26 16:51:40 +0530

    prep files

---
+++ src/components/Articles.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import SkeletonArticle from '../skeletons/SkeletonArticle'
+
+const Articles = () => {
+  const [articles, setArticles] = useState(null)
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const data = await res.json()
+      setArticles(data)
+    }, 5000)
+  })
+
+  return (
+    <div className='articles'>
+      <h2>Articles</h2>
+
+      {articles?.map((article) => (
+        <a href='/'>
+          <div className='article' key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.body}</p>
+          </div>
+        </a>
+      ))}
+
+      {!articles && [1, 2, 3, 4, 5].map((n) => <SkeletonArticle key={n} theme='dark' />)}
+    </div>
+  )
+}
+
+export default Articles
